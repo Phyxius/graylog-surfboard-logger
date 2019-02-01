97cc33c419bf01ebf837ade0d9e4ee931bf3b9bb
Shea Polansky <shea@polansky.co>
2019-01-31 19:50:53 -0800

    Now sends logs

--- surfboard-log-scraper.py
+++ surfboard-log-scraper.py
@@ -4,6 +4,16 @@ import sys
 from bs4 import BeautifulSoup
 import urllib.request
 import json 
+import os.path
+from pygelf import GelfUdpHandler
+import logging
+
+priorityMapping = {
+	'6-Notice': logging.INFO,
+	'5-Warning': logging.WARNING,
+	'4-Error': logging.ERROR,
+	'3-Critical': logging.CRITICAL,
+}
 
 soup = BeautifulSoup(urllib.request.urlopen(sys.argv[1]), 'html.parser')
 
@@ -17,7 +27,31 @@ for row in rows:
     cols = [ele.text.strip() for ele in cols]
     data.append([ele for ele in cols if ele]) # Get rid of empty values
 
+last_error = []
+try:
+	if os.path.isfile('last-error.json'):
+		with open('last-error.json', 'r') as f:
+			last_error = json.loads(f.read())
+except json.decoder.JSONDecodeError:
+	print("Couldn't parse last-error.json, assuming empty...", file=sys.stderr)
+with open('last-error.json', 'w') as f:
+	f.write(json.dumps(data[0]))
 
+new_errors = []
+for row in data:
+	if row == last_error: break
+	new_errors.append(row)
 
-print(data)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+handler = GelfUdpHandler(sys.argv[2], port=int(sys.argv[3]), include_extra_fields=True)
+handler.domain = sys.argv[4]
+logger.addHandler(handler)
 
+for reportedTime,originalPriority,code,message in new_errors:
+	priority = priorityMapping[originalPriority] if originalPriority in priorityMapping else logging.WARNING
+	logger.log(priority, message, extra={
+		'originalPriority': originalPriority,
+		'code': code,
+		'reportedTime': reportedTime,
+		})
